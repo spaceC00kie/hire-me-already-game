40e802c11cc87a74bc820e55a90466e2e2135c0e
spaceC00kie <kirstie317@gmail.com>
2025-02-04 14:10:02 -0500

    job queue sets job list, which maps to job cards

--- src/components/applyWithin/ApplyWithin.tsx
+++ src/components/applyWithin/ApplyWithin.tsx
@@ -1,21 +1,15 @@
-import {
-  BiGhost,
-  BiPlanet,
-  BiMoon,
-  BiBot,
-  BiCookie,
-  BiMeteor,
-  BiRocket,
-  BiChip,
-  BiAtom,
-} from "react-icons/bi"
+import { BiGhost } from "react-icons/bi"
 import { JobCard } from "../body/tiles/JobCard"
 import { useState } from "react"
+import { jobQueue } from "./JobQueue"
+import { Job } from "../../interfaces/Job"
 
 interface Props {}
 
 export const ApplyWithin: React.FC<Props> = ({}) => {
   const [applied, setApplied] = useState<boolean>(false)
+
+  const [jobsList, setJobsList] = useState<Job[]>(jobQueue)
 
   return (
     <div className="flex h-full w-1/2 flex-col gap-2">
@@ -23,26 +17,9 @@ export const ApplyWithin: React.FC<Props> = ({}) => {
         <BiGhost size={52} color="white" />
       </div>
       <div className="flex w-full flex-col items-center gap-2">
-        <JobCard
-          icon={<BiPlanet size={32} />}
-          benefitsList="Starting at $40/hr"
-          setApplied={setApplied}
-        />
-        <JobCard icon={<BiMoon size={32} />} setApplied={setApplied} />
-        <JobCard
-          icon={<BiBot size={32} />}
-          benefitsList="$89.2K/yr - $149.8K/yr · Medical · 401k"
-          setApplied={setApplied}
-        />
-        <JobCard icon={<BiCookie size={32} />} setApplied={setApplied} />
-        <JobCard icon={<BiMeteor size={32} />} setApplied={setApplied} />
-        <JobCard icon={<BiRocket size={32} />} setApplied={setApplied} />
-        <JobCard
-          icon={<BiChip size={32} />}
-          benefitsList="$90K/yr - $120K/yr · 401k, Medical, Vision, Dental"
-          setApplied={setApplied}
-        />
-        <JobCard icon={<BiAtom size={32} />} setApplied={setApplied} />
+        {jobsList.map((job) => (
+          <JobCard job={job} setApplied={setApplied} />
+        ))}
       </div>
     </div>
   )
--- src/components/body/tiles/JobCard.tsx
+++ src/components/body/tiles/JobCard.tsx
@@ -1,23 +1,20 @@
+import { Job } from "../../../interfaces/Job"
+
 interface Props {
-  icon?: React.ReactNode
-  benefitsList?: string
+  job: Job
   setApplied: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export const JobCard: React.FC<Props> = ({
-  icon,
-  benefitsList,
-  setApplied,
-}) => {
+export const JobCard: React.FC<Props> = ({ job, setApplied }) => {
   return (
-    <div className="flex h-32 w-3/4 rounded-md border text-white">
-      <div className="grid w-1/12 place-content-center">{icon}</div>
+    <div key={job.id} className="flex h-32 w-3/4 rounded-md border text-white">
+      <div className="grid w-1/12 place-content-center">{job.icon}</div>
       <div className="flex w-8/12 flex-col py-2">
-        <div className="px-3 text-xl">Job title</div>
-        <div className="px-3">Company</div>
-        <div className="px-3">Location</div>
-        <div className="px-3 text-sm">{benefitsList}</div>
-        <div className="px-3 text-sm text-stone-300">This is an ad</div>
+        <div className="px-3 text-xl">{job.title}</div>
+        <div className="px-3">{job.company}</div>
+        <div className="px-3">{job.location}</div>
+        <div className="px-3 text-sm">{job.benefits}</div>
+        <div className="px-3 text-sm text-stone-300">{job.tag}</div>
       </div>
       <div className="flex w-3/12 items-end justify-end px-3 py-3 ">
         <button
