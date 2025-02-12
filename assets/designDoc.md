2e65acc0741837917e403c7fd7be798409d58723
spaceC00kie <kirstie317@gmail.com>
2025-02-11 19:19:20 -0500

    add job prop and min-h to email card refine design doc

--- assets/designDoc.md
+++ assets/designDoc.md
@@ -13,11 +13,15 @@ Each job will have an id, title, icon, company name, location, a time to wait be
 2 lists(shown to users): jobs and emails  
 2 queues(used to populate lists): contain jobs and emails
 
-Will use Firestore to store the queues for users who are not logged in
-If users aren't logged in, will use useLocalStorage hook
+Jobs are popped from the job queue to the job list when user clicks "Search"
+Jobs are added to the email queue when the user clicks "Apply" on the job card  
+Jobs are popped off the email queue and added to the email list after their "waitTime" has passed  
+Jobs will be removed from the email list when the user clicks the trash button in the email card  
+Will pass the list of emails into onClick's callback, to be used to unmount the current item(by its id) from the email list  
 
-Jobs will be removed from the email list with a button the user clicks
-Will pass the list of emails into onClick's callback, to be used to unmount the current item(by its id) from the email list
+
+Will use Firestore to store the queues for users who are not logged in  
+If users aren't logged in, will use useLocalStorage hook
 
 ###### job queue => job list => email queue => email list => scorecard
 
--- src/components/body/tiles/EmailCard.tsx
+++ src/components/body/tiles/EmailCard.tsx
@@ -1,22 +1,25 @@
 import { BiTrash } from "react-icons/bi"
+import { Job } from "../../../interfaces/Job"
 
-interface Props {}
+interface Props {
+  job: Job
+}
 
-export const EmailCard: React.FC<Props> = ({}) => {
+export const EmailCard: React.FC<Props> = ({ job }) => {
   return (
-    <div className="flex h-52 w-full rounded-md border py-2">
+    <div className="min-h-52 flex w-full rounded-md border py-2">
       <div className="flex flex-col text-white">
         <div className="flex justify-between">
           <div className="px-3 py-2 text-2xl">Rejected!</div>
           <button className="flex justify-end rounded-md p-3 hover:border">
-            <BiTrash size={18} />
+            <BiTrash size={18} onClick={() => {}} />
           </button>
         </div>
         <div className="px-3 pb-1">Greetings Faceless Candidate,</div>
-        <div className="px-3">
-          You've been rejected for (job title for which you just applied).
+        <div className="px-3 py-1">
+          You've been rejected for our {job.title} role.
         </div>
-        <div className="px-3">
+        <div className="px-3 py-1">
           We hired someone else. Or we were kidding about hiring anyone to begin
           with. Sorry. Best of luck!
         </div>
