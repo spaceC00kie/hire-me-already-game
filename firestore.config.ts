e4823d25948bd423a3b6fa724f9717c7a64e1b87
spaceC00kie <kirstie317@gmail.com>
2025-03-31 16:47:22 -0400

    update Firebase project configuration to 'hire-me-already-game'

--- firestore.config.ts
+++ firestore.config.ts
@@ -1,11 +1,15 @@
 import { initializeApp } from "firebase/app"
+// import { getAnalytics } from "firebase/analytics"
 
-export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyDmBaAXrP8FGKbchlTHTZzKCOZqhZSH6AY",
-  authDomain: "has-the-dog-been-fed-f9102.firebaseapp.com",
-  projectId: "has-the-dog-been-fed-f9102",
-  storageBucket: "has-the-dog-been-fed-f9102.appspot.com",
-  messagingSenderId: "46976099487",
-  appId: "1:46976099487:web:8e17ada9ee2cf899bfaeaa",
-  measurementId: "G-QNVCXH0G3Q"
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyBxIPko1qdSwSO0T4dzeYzNYWWEHkg8AwU",
+  authDomain: "hire-me-already-game.firebaseapp.com",
+  projectId: "hire-me-already-game",
+  storageBucket: "hire-me-already-game.firebasestorage.app",
+  messagingSenderId: "22543170029",
+  appId: "1:22543170029:web:014d51f44b90a67e24cfe4",
+  measurementId: "G-TTG7FGY6M5",
+}
+
+export const firebaseApp = initializeApp(firebaseConfig)
+// export const analytics = getAnalytics(firebaseApp)
